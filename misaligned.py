f626da4743d6f59977f56978a101a39f4496573d
Vishwalekha2001 <vishwalekhad@gmail.com>
2025-08-05 10:23:22 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -2,8 +2,8 @@
 def print_color_map():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-    for i, major in enumerate(major_colors):
-        for j, minor in enumerate(minor_colors):
+    for i=1, major in enumerate(major_colors):
+        for j=1, minor in enumerate(minor_colors):
             print(f'{i * 5 + j} | {major} | {minor}')
     return len(major_colors) * len(minor_colors)
 
@@ -11,3 +11,4 @@ def print_color_map():
 result = print_color_map()
 assert(result == 25)
 print("All is well (maybe!)")
+
